fd3ab7abf348a6dd0688774b9dbf68e30f95a5af
koo <kooperativer@gmail.com>
2021-03-24 23:20:36 +0100

    Initial Commit

--- src/main/java/de/koo/other/StringManager.java
+++ src/main/java/de/koo/other/StringManager.java
@@ -7,7 +7,7 @@ package de.koo.other;
  */
 public class StringManager {
 
-    public static final String TOKEN = "ODI0MzczMTI5MjcwNjU3MDM2.YFubTg.89ZViCdWObcM6FZLKOrtGvP3TZg";
+    public static final String TOKEN = "null";
     public static final String PREFIX = "yg!";
     public static final String CMD_NOT_FOUND = "The command you where trying to use don't exists.";
     public static final String USER_NOT_FOUND = "The User you where trying to mention don't exists.";
